5ed8d5686d475a513907bd9e597853a55bd60330
Hazael Vite <14224891+HazaelVite@users.noreply.github.com>
2022-10-12 17:20:14 -0500

    Adding algoritmo

--- passwordGenerator/src/index.js
+++ passwordGenerator/src/index.js
@@ -29,3 +29,15 @@ const letters = [
 
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 const symbols = ["'", ":", "!", "@", "#", "$", "^", ")", "&", "*", "%", "-"];
+
+const arrayOfArrays = [letters, numbers, symbols];
+
+let passwordLength = 10;
+
+for (let i = 0; i < passwordLength; i++) {
+  const myArr = arrayOfArrays;
+}
+
+const getRandomNumber = (min, max) => {
+
+}
