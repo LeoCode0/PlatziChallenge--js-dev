f717f96f1b6d1ef1d0a05f7e3e5e03dbfb9b06bb
Leonardo Espinoza <leoespinozidk@gmail.com>
2022-10-12 17:39:02 -0500

    add functions

--- passwordGenerator/src/index.js
+++ passwordGenerator/src/index.js
@@ -1,3 +1,6 @@
+const paragraphPassword = document.querySelector("#password");
+const form = document.querySelector("#form");
+
 const letters = [
   "a",
   "b",
@@ -29,3 +32,30 @@ const letters = [
 
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 const symbols = ["'", ":", "!", "@", "#", "$", "^", ")", "&", "*", "%", "-"];
+
+const arrayOfArrays = [letters, numbers, symbols];
+
+let passwordLength = 10;
+
+function generatePassword() {
+  let strongPassword = [];
+  for (let i = 0; i < passwordLength; i++) {
+    const myArr = arrayOfArrays[getRandomNumber(0, arrayOfArrays.length - 1)];
+    const randomCharacter = myArr[getRandomNumber(0, myArr.length - 1)];
+
+    strongPassword.push(randomCharacter);
+  }
+
+  strongPassword = strongPassword.join("");
+  paragraphPassword.innerText = `${paragraphPassword.textContent} ${strongPassword}`;
+  console.log(strongPassword);
+}
+
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1));
+}
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  console.log(event.target.length.value);
+});
