cac39a029feabc21dabb038c464967c9bc03ec9e
Hazael Vite <14224891+HazaelVite@users.noreply.github.com>
2022-10-12 18:17:22 -0500

    Mejoras primera clase

--- passwordGenerator/src/index.js
+++ passwordGenerator/src/index.js
@@ -33,22 +33,30 @@ const letters = [
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 const symbols = ["'", ":", "!", "@", "#", "$", "^", ")", "&", "*", "%", "-"];
 
-const arrayOfArrays = [letters, numbers, symbols];
+// let passwordLength = 10;
 
-let passwordLength = 10;
-
-function generatePassword() {
+function generatePassword(passwordLength, opciones ) {
+  console.log(passwordLength);
+  const arrayOfArrays = [];
+  if(opciones.letters) {
+    arrayOfArrays.push(letters);
+  }
+  if(opciones.numbers) {
+    arrayOfArrays.push(numbers);
+  }
+  if(opciones.symbols) {
+    arrayOfArrays.push(symbols);
+  }
   let strongPassword = [];
   for (let i = 0; i < passwordLength; i++) {
     const myArr = arrayOfArrays[getRandomNumber(0, arrayOfArrays.length - 1)];
-    const randomCharacter = myArr[getRandomNumber(0, myArr.length - 1)]
+    const randomCharacter = myArr[getRandomNumber(0, myArr.length - 1)];
 
     strongPassword.push(randomCharacter);
   }
 
   strongPassword = strongPassword.join("")
-  console.log(strongPassword);
-  paragraphPassword.innerText = `${paragraphPassword.textContent}  ${strongPassword}`
+  paragraphPassword.innerText = `Tu contraseña es: ${strongPassword}`
 }
 
 function getRandomNumber(min, max) {
@@ -57,5 +65,15 @@ function getRandomNumber(min, max) {
 
 form.addEventListener("submit", (e) => {
   e.preventDefault()
-  console.log("Se esta generando la contraseña");
+  const formElement = e.target
+  const passwordLength = formElement.length.value;
+  // console.log("Se esta generando la contraseña");
+  const checks = {
+    letters: formElement.letters.checked,
+    words: formElement.words.checked,
+    numbers: formElement.numbers.checked,
+    symbols: formElement.symbols.checked,
+  }
+
+  generatePassword(passwordLength, checks)
 })
