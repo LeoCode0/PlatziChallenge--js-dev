cb61c7c7f8e658c336e7c9a30b3fb27a9e1eb853
Leonardo Espinoza <leoespinozidk@gmail.com>
2022-10-12 17:54:12 -0500

    Clean code

--- passwordGenerator/src/index.js
+++ passwordGenerator/src/index.js
@@ -33,11 +33,23 @@ const letters = [
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 const symbols = ["'", ":", "!", "@", "#", "$", "^", ")", "&", "*", "%", "-"];
 
-const arrayOfArrays = [letters, numbers, symbols];
+function generatePassword(passwordLength, botonsitos) {
+  const arrayOfArrays = [];
 
-let passwordLength = 10;
+  if (botonsitos.letters) {
+    arrayOfArrays.push(letters);
+  }
 
-function generatePassword() {
+  if (botonsitos.numbers) {
+    arrayOfArrays.push(numbers);
+  }
+
+  if (botonsitos.symbols) {
+    arrayOfArrays.push(symbols);
+  }
+
+  console.log(arrayOfArrays);
+
   let strongPassword = [];
   for (let i = 0; i < passwordLength; i++) {
     const myArr = arrayOfArrays[getRandomNumber(0, arrayOfArrays.length - 1)];
@@ -47,8 +59,7 @@ function generatePassword() {
   }
 
   strongPassword = strongPassword.join("");
-  paragraphPassword.innerText = `${paragraphPassword.textContent} ${strongPassword}`;
-  console.log(strongPassword);
+  paragraphPassword.innerText = `Aquí aparecerá tu contraseña ${strongPassword}`;
 }
 
 function getRandomNumber(min, max) {
@@ -57,5 +68,14 @@ function getRandomNumber(min, max) {
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  console.log(event.target.length.value);
+  const formElement = event.target;
+  const passwordLength = formElement.length.value;
+  const checks = {
+    letters: formElement.letters.checked,
+    words: formElement.words.checked,
+    numbers: formElement.numbers.checked,
+    symbols: formElement.symbols.checked,
+  };
+
+  generatePassword(passwordLength, checks);
 });
