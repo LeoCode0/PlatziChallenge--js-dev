965fcb60d2679592db48fb6d76610edc0097a64b
Lisbeth Quintero <lquintero28@live.com>
2022-10-27 18:01:49 -0500

    BEM method application, redesigned styles, social networks were added. In JS change the message of the copy button and API change.

--- passwordGenerator/src/index.js
+++ passwordGenerator/src/index.js
@@ -1,130 +1,139 @@
-const paragraphPassword = document.querySelector("#password");
-const form = document.querySelector("#form");
-const buttonCopy = document.querySelector("#button-copy");
-const inputLength = document.querySelector("#input-length");
-const passwordLengthParagraph = document.querySelector("#password-length");
+const inputLength = document.querySelector(".input-length");
+const passwordLength = document.querySelector(".password-length");
+const form = document.querySelector(".form-container");
+const password = document.querySelector(".password");
+const buttonCopy = document.querySelector(".button");
 
-const API = "https://goquotes-api.herokuapp.com/api/v1/random?count=5";
+const API = "https://random-word-api.herokuapp.com/all";
 
 const letters = [
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z",
-];
-
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+const numbers =[1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 const symbols = ["'", ":", "!", "@", "#", "$", "^", ")", "&", "*", "%", "-"];
 let words = [];
 
-function generatePassword(passwordLength, botonsitos) {
-  let arrayOfArrays = [];
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1));
+}
 
-  if (botonsitos.letters) {
-    arrayOfArrays.push(letters);
-  }
+function generatePassword(passwordLengthChosen, checkBoxChosen) {
 
-  if (botonsitos.numbers) {
-    arrayOfArrays.push(numbers);
-  }
+    let arrayOfArrays = [];
 
-  if (botonsitos.symbols) {
-    arrayOfArrays.push(symbols);
-  }
+    if (checkBoxChosen.letters) {
+        arrayOfArrays.push(letters);
+      }
 
-  if (botonsitos.words) {
-    arrayOfArrays = [];
-    arrayOfArrays.push(words);
-  }
+    if (checkBoxChosen.numbers) {
+      arrayOfArrays.push(numbers);
+    }
 
-  let strongPassword = [];
-  for (let i = 0; i < passwordLength; i++) {
-    const myArr = arrayOfArrays[getRandomNumber(0, arrayOfArrays.length - 1)];
+    if (checkBoxChosen.symbols) {
+      arrayOfArrays.push(symbols);
+    }
 
-    const randomCharacter = myArr[getRandomNumber(0, myArr.length - 1)];
+    if (checkBoxChosen.words) {
+      arrayOfArrays = [];
+      arrayOfArrays.push(words);
+    }
 
-    strongPassword.push(randomCharacter);
-  }
+    let strongPassword = [];
+    for (let i = 0; i < passwordLengthChosen; i++) {
+      const myArr = arrayOfArrays[getRandomNumber(0, arrayOfArrays.length - 1)];
+      const randomCharacter = myArr[getRandomNumber(0, myArr.length - 1)];
+      strongPassword.push(randomCharacter);
+    }
 
-  if (botonsitos.words) {
-    strongPassword = strongPassword.join("-");
-  } else {
-    strongPassword = strongPassword.join("");
-  }
-  paragraphPassword.value = strongPassword;
+    if (checkBoxChosen.words) {
+      strongPassword = strongPassword.join("-");
+    } else {
+      strongPassword = strongPassword.join("");
+    }
+
+
+    password.innerText = strongPassword;
 }
 
 function fetchData(API) {
   fetch(API)
-    .then((response) => response.json())
+    .then((response) => response.json()) // convierte la api a json
     .then((data) => {
-      words = data.quotes.map((quote) => quote.text);
-      words = words.join("").split(" ").sort();
+      words = data
     });
 }
 
 fetchData(API);
 
-function getRandomNumber(min, max) {
-  return Math.floor(Math.random() * (max - min + 1));
-}
-
-function copyToClipboard(target) {
-  const element = document.querySelector(target);
-  const value = element.value;
-  if (value.length === 0) {
-    alert("Tienes que generar una contraseña");
-  } else {
-    window.navigator.clipboard.writeText(value);
-    alert("Copiaste la contraseña");
-  }
+function CopyToClipboard(id)
+{
+  const r = document.createRange();
+  r.selectNode(document.querySelector(id));
+  window.getSelection().removeAllRanges();
+  window.getSelection().addRange(r);
+  if (password.innerText == 0) {
+      swal("Primero genera una contraseña");
+      } else {
+        window.navigator.clipboard.writeText(r);
+        swal("Copiaste la contraseña");
+        window.getSelection().removeAllRanges();
+        
+      }
 }
 
 form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  const formElement = event.target;
-  const passwordLength = formElement.length.value;
-  const checks = {
-    letters: formElement.letters.checked,
-    words: formElement.words.checked,
-    numbers: formElement.numbers.checked,
-    symbols: formElement.symbols.checked,
-  };
+    event.preventDefault();
+    const formElement = event.target;
+    const passwordLength = formElement.length.value;
 
-  if (checks.words) {
-    formElement.letters.checked = false;
-  }
+    const checks = {
+      letters: formElement.letters.checked,
+      words: formElement.words.checked,
+      numbers: formElement.numbers.checked,
+      symbols: formElement.symbols.checked,
+    };
 
-  generatePassword(passwordLength, checks);
-  buttonCopy.disabled = false;
-});
-
+    if (checks.words) {
+      formElement.letters.checked = false;
+      formElement.numbers.checked = false;
+      formElement.symbols.checked = false;
+      }
+      
+    generatePassword(passwordLength, checks);
+    buttonCopy.disabled = false;
+  });
+  
 buttonCopy.addEventListener("click", () => {
-  copyToClipboard("#password");
-});
+    CopyToClipboard(".password");
+    return false;
+  });
+
 
 inputLength.addEventListener("input", (e) => {
-  passwordLengthParagraph.innerText = e.target.value;
+  passwordLength.innerText = e.target.value;
 });
